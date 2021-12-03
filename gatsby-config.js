a30c3fe7ab51a03e664f43dad46bc58d47871b6d
Ankit Chouhan <ankitchouhan1020@gmail.com>
2021-12-03 06:14:20 +0000

    feat: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,10 +44,32 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-ZBVD46LDR7", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-omni-font-loader`,
       options: {
         enableListener: true,
-        preconnect: [`https://fonts.gstatic.com`],
+        preconnect: [ `https://fonts.gstatic.com` ],
         interval: 300,
         timeout: 30000,
         // If you plan on changing the font you'll also need to adjust the Theme UI config to edit the CSS
@@ -110,7 +132,7 @@ module.exports = {
                   excerpt: post.excerpt,
                   url,
                   guid: url,
-                  custom_elements: [{ "content:encoded": content }],
+                  custom_elements: [ { "content:encoded": content } ],
                 }
               }),
             query: `
